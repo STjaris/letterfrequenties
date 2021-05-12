d88a9c0eea3746255ba829b1a40425763387ffc9
STjaris <43371513+STjaris@users.noreply.github.com>
2021-05-12 16:47:31 +0200

    clfvkj;fnbojikxfcdgvnbjoifng xpjkb

--- src/main/java/percentages/percentagesMapper.java
+++ src/main/java/percentages/percentagesMapper.java
@@ -17,53 +17,69 @@ public class percentagesMapper extends Mapper<LongWritable, Text, Text, DoubleWr
     public void map(LongWritable Key, Text value, Context context) throws IOException, InterruptedException {
         String[] tokens = value.toString().split("\\s");
 
-        ArrayList<String> bigram = new ArrayList<>();
+        ArrayList<String> bigramList = new ArrayList<>();
         ArrayList<Integer> bigramCount = new ArrayList<>();
 
-        ArrayList<String> total = new ArrayList<>();
+        ArrayList<String> totalList = new ArrayList<>();
         ArrayList<Integer> totalCount = new ArrayList<>();
 
+        ArrayList<String> returnList = new ArrayList<>();
+        ArrayList<Double> returnCount = new ArrayList<>();
+
+        String s = "";
+        int iq = 0;
 
         // SET TOKENS IN MAP
         for (int i = 0; i < tokens.length; i++) {
             if (i % 2 == 1) {
                 bigramCount.add(Integer.parseInt(tokens[i]));
             } else {
-                bigram.add(tokens[i]);
+                bigramList.add(tokens[i]);
             }
         }
 
-        for(int i = 0; i < bigram.size(); i++){
-            if(bigram.get(i).length() == 1){
+        // SEPERATE BIGRAM FROM COUNT
+        for(int i = 0; i < bigramList.size(); i++){
+            if(bigramList.get(i).length() == 1){
                 // ADD TO TOTAL LIST
-                total.add(bigram.get(i));
+                totalList.add(bigramList.get(i));
                 totalCount.add(bigramCount.get(i));
 
                 // REMOVE FROM BIGRAM LIST
-                bigram.remove(i);
+                bigramList.remove(i);
                 bigramCount.remove(i);
             }
         }
 
-        for(String s : bigram){
-            System.out.println(s);
-        }
-
-        for(int s : bigramCount){
-            System.out.println(s);
-        }
-
-
-//        for(String s : bigram){
-//            System.out.println(s);
-//        }
-
-
-
-
-        // SPLIT BIGRAM FROM TOTALCOUNT
-
         // DIVIDE BIGRAM COUNT BY TOTAL AMOUNT
+        for(int i = 0; i < bigramList.size(); i++){
+
+            s = String.valueOf(bigramList.get(i).charAt(0));
+            iq = i;
+
+        }
+
+        for(String st : totalList){
+            if(st.equals(s)){
+                int index = totalList.indexOf(s);
+                double d = bigramCount.get(iq) / totalCount.get(index);
+                returnList.add(bigramList.get(iq));
+                returnCount.add(d);
+            }
+        }
+
+
+
+
+        for(String st : returnList){
+            System.out.println(st);
+        }
+
+        for(double st : returnCount){
+            System.out.println(st);
+        }
+
+
 
 
 
