052726a1ac5f290c7be3584b7f64745e2c532704
STjaris <43371513+STjaris@users.noreply.github.com>
2021-05-12 16:18:58 +0200

    Update percentagesMapper.java

--- src/main/java/percentages/percentagesMapper.java
+++ src/main/java/percentages/percentagesMapper.java
@@ -1,15 +1,14 @@
 package percentages;
 
 import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.checkerframework.checker.units.qual.A;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 
@@ -18,48 +17,53 @@ public class percentagesMapper extends Mapper<LongWritable, Text, Text, DoubleWr
     public void map(LongWritable Key, Text value, Context context) throws IOException, InterruptedException {
         String[] tokens = value.toString().split("\\s");
 
-        ArrayList bigram = new ArrayList<>();
-        ArrayList bigramCount = new ArrayList<>();
+        ArrayList<String> bigram = new ArrayList<>();
+        ArrayList<Integer> bigramCount = new ArrayList<>();
 
-        Map<String, String> bigramMap = new HashMap<>();
-        Map<String, String> totalMap = new HashMap<>();
-        Map<String, Double> returnMap = new HashMap<>();
+        ArrayList<String> total = new ArrayList<>();
+        ArrayList<Integer> totalCount = new ArrayList<>();
+
 
         // SET TOKENS IN MAP
         for (int i = 0; i < tokens.length; i++) {
             if (i % 2 == 1) {
-                bigramMap.put(tokens[i - 1], tokens[i]);
+                bigramCount.add(Integer.parseInt(tokens[i]));
+            } else {
+                bigram.add(tokens[i]);
             }
         }
+
+        for(int i = 0; i < bigram.size(); i++){
+            if(bigram.get(i).length() == 1){
+                // ADD TO TOTAL LIST
+                total.add(bigram.get(i));
+                totalCount.add(bigramCount.get(i));
+
+                // REMOVE FROM BIGRAM LIST
+                bigram.remove(i);
+                bigramCount.remove(i);
+            }
+        }
+
+        for(String s : bigram){
+            System.out.println(s);
+        }
+
+        for(int s : bigramCount){
+            System.out.println(s);
+        }
+
+
+//        for(String s : bigram){
+//            System.out.println(s);
+//        }
+
+
+
 
         // SPLIT BIGRAM FROM TOTALCOUNT
-        for (Map.Entry<String, String> entry : bigramMap.entrySet()) {
-            if (entry.getKey().length() == 1) {
-
-                totalMap.put(entry.getKey(), entry.getValue());
-                bigramMap.remove(entry.getKey());
-            }
-
-
-        }
 
         // DIVIDE BIGRAM COUNT BY TOTAL AMOUNT
-        for (Map.Entry<String, String> o : bigramMap.entrySet()) {
-
-            String s = String.valueOf(o.getKey().charAt(0));
-
-            if(totalMap.containsKey(s)){
-                double d = Integer.parseInt(o.getValue()) / Integer.parseInt(totalMap.get(s));
-
-                returnMap.put(o.getKey(), d);
-            }
-        }
-
-        for(Map.Entry<String, Double> entry : returnMap.entrySet()){
-            System.out.println(entry);
-            context.write(new Text(entry.getKey()), new DoubleWritable(entry.getValue()));
-
-        }
 
 
 
