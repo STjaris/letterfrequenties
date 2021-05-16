ab85d3fba1cd1324347db4c05e2424478d7e4066
STjaris <43371513+STjaris@users.noreply.github.com>
2021-05-16 12:50:03 +0200

    updated percentagesMapper to filter on context;

--- src/main/java/percentages/percentagesMapper.java
+++ src/main/java/percentages/percentagesMapper.java
@@ -12,39 +12,28 @@ import java.util.Map;
 
 public class percentagesMapper extends Mapper<LongWritable, Text, Text, DoubleWritable> {
 
-    static Map<String, String> completeMap = new HashMap<>();
-    static Map<String, String> bigramMap = new HashMap<>();
-    static Map<String, String> totalMap = new HashMap<>();
+    static Map<String, Double> bigramMap = new HashMap<>();
+    static Map<String, Double> totalMap = new HashMap<>();
 
     public void map(LongWritable Key, Text value, Context context) throws IOException, InterruptedException {
-        String[] tokens = value.toString().split("\\s");
+        String[] tokens = value.toString().split("\\n");
 
         // SET TOKENS IN MAP
-        for (int i = 0; i < tokens.length; i++) {
-            if (i % 2 == 1) {
-                completeMap.put(tokens[i - 1], tokens[i]);
-            }
-        }
-
-        // SPLIT BIGRAM FROM FIRSTCHAR
-        for (Map.Entry<String, String> entry : completeMap.entrySet()) {
-            if (entry.getKey().length() == 1) {
-
-                totalMap.put(entry.getKey(), entry.getValue());
-            } else {
-                bigramMap.put(entry.getKey(), entry.getValue());
+        for (String s : tokens) {
+            double probability = Double.parseDouble(s.split("\\s+")[1]);
+            String bigram = s.split("\\s+")[0];
+            if (context.getInputSplit().toString().contains("createBigram_output")) {
+                bigramMap.put(bigram, probability);
+            } else if (context.getInputSplit().toString().contains("firstLetterCount_output")) {
+                totalMap.put(bigram, probability);
             }
         }
 
         // DIVIDE BIGRAM COUNT BY TOTAL AMOUNT
-        for (Map.Entry<String, String> entry : bigramMap.entrySet()) {
-            for (Map.Entry<String, String> o : totalMap.entrySet()) {
+        for (Map.Entry<String, Double> entry : bigramMap.entrySet()) {
+            for (Map.Entry<String, Double> o : totalMap.entrySet()) {
                 if (entry.getKey().charAt(0) == o.getKey().charAt(0)) {
-                    double num = Double.parseDouble(entry.getValue());
-                    double div = Double.parseDouble(o.getValue());
-
-                    double i = num / div;
-
+                    double i = entry.getValue() / o.getValue();
                     context.write(new Text(entry.getKey()), new DoubleWritable(i));
                 }
             }
